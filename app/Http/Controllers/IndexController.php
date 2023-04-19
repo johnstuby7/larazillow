4a5ab2a2a61bf5b6cf6cfe07bd644f93dd6e144d
John Stuby <johnstuby7@gmail.com>
2023-04-19 10:31:11 -0600

    added in install tips file, updated indexcontroller with code for alternative to tinker, updated listing model logic

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers;
 
-
+use App\Models\Listing;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
    public function index() 
    {
+    // dd(Listing::all());
+    // Listing::create([
+    //     'beds' => 2, 'baths' => 2, 'area' => 100, 'city' => 'North', 'street' => 'Tinker st', 'street_nr' => 20, 'code' => 'TS', 'price' => 200_000
+    // ]);
+
     return inertia(
         'Index/Index',
         [
