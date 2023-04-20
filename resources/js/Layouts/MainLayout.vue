f7a6aa53e25586aa23c941ccb9f130ac3636fba5
John Stuby <johnstuby7@gmail.com>
2023-04-20 10:51:58 -0600

    Added in button to header

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,5 +1,4 @@
 <template>
-  <Link href="/listing/create">New Listing</Link>
   <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 w-full">
     <div class="container mx-auto">
       <nav class="p-4 flex items-center justify-between">
@@ -10,17 +9,20 @@
           <Link :href="route('listing.index')">LaraZillow</Link>&nbsp;
         </div>
         <div>
-
+          <Link :href="route('listing.create')" class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md">
+            + New Listing
+          </Link>
         </div>
       </nav>
     </div>
   </header>
 
-  <div v-if="flashSuccess" class="success">
-    {{ flashSuccess }}
-  </div>
-
-  <slot>Default</slot>
+  <main class="container mx-auto p-4">
+    <div v-if="flashSuccess" class="success">
+      {{ flashSuccess }}
+    </div>
+    <slot>Default</slot>
+  </main>
 </template>
 <script setup>
   import {computed} from 'vue'
