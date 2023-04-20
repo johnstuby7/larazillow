ac16fc4dd110355b9bd3f7859b68384fbf9e19ee
John Stuby <johnstuby7@gmail.com>
2023-04-20 10:48:12 -0600

    adding in a header part 1

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,6 +1,20 @@
 <template>
-  <Link href="/listing">Listings</Link>&nbsp;
   <Link href="/listing/create">New Listing</Link>
+  <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 w-full">
+    <div class="container mx-auto">
+      <nav class="p-4 flex items-center justify-between">
+        <div class="text-lg font-medium">
+          <Link :href="route('listing.index')">Listings</Link>&nbsp;
+        </div>
+        <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
+          <Link :href="route('listing.index')">LaraZillow</Link>&nbsp;
+        </div>
+        <div>
+
+        </div>
+      </nav>
+    </div>
+  </header>
 
   <div v-if="flashSuccess" class="success">
     {{ flashSuccess }}
@@ -18,9 +32,3 @@
   )
 
 </script>
-<style scoped>
-.success {
-  background-color: green;
-  color: white;
-}
-</style>
