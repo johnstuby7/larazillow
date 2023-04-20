d3a1bc8864ff88406bfcdeee824ad4bdc015dde3
John Stuby <johnstuby7@gmail.com>
2023-04-20 09:06:54 -0600

    added in edit logic and page

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,7 +1,6 @@
 <template>
-  <Link href="/">Main Page</Link>&nbsp;
-  <Link href="/hello">Show Page</Link>
-  <!-- <div>The page with time {{ timer }}</div> -->
+  <Link href="/listing">Listings</Link>&nbsp;
+  <Link href="/listing/create">New Listing</Link>
 
   <div v-if="flashSuccess" class="success">
     {{ flashSuccess }}
@@ -13,15 +12,11 @@
   import {computed} from 'vue'
   import {Link, usePage} from '@inertiajs/vue3'
 
-  // page.props.value.flash.success
   const page = usePage()
   const flashSuccess = computed(() => 
     page.props.flash.success, 
   )
-  // import { ref } from 'vue';
 
-  // const timer = ref(0)
-  // setInterval(() => timer.value++, 1000)
 </script>
 <style scoped>
 .success {
