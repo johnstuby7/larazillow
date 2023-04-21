cdab7a1f096ec9cb21b1c676c9bf9305a629b8c7
John Stuby <johnstuby7@gmail.com>
2023-04-21 14:38:14 -0600

    Added in basic logout functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,29 +8,34 @@ use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    public function create() 
+    public function create()
     {
         return inertia('Auth/Login');
     }
 
-    public function store(Request $request) 
+    public function store(Request $request)
     {
-        if(!Auth::attempt($request->validate([
+        if (!Auth::attempt($request->validate([
             'email' => 'required|string|email',
             'password' => 'required|string'
         ]), true)) {
             throw ValidationException::withMessages([
-                'email' => 'Authentication Failed'
+                'email' => 'Authentication failed'
             ]);
         }
 
         $request->session()->regenerate();
-        
+
         return redirect()->intended('/listing');
     }
 
-    public function destroy() 
+    public function destroy(Request $request)
     {
-        
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect()->route('listing.index');
     }
 }
