d80b8d723869c4944cd0c55024629dbb10e6fa20
John Stuby <johnstuby7@gmail.com>
2023-04-21 16:04:43 -0600

    added in user registration page

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserAccountController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,3 +34,6 @@ Route::post('login', [AuthController::class, 'store'])
   ->name('login.store');
 Route::delete('logout', [AuthController::class, 'destroy'])
   ->name('logout');
+
+Route::resource('user-account', UserAccountController::class)
+  ->only(['create']);
