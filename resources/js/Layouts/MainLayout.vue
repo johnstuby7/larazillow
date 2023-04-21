ba7f362992ec864a8c0a42df43029c7556b674a9
John Stuby <johnstuby7@gmail.com>
2023-04-21 14:28:06 -0600

    Adding the currently logged in user to the top right of the header

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -3,20 +3,23 @@
     <div class="container mx-auto">
       <nav class="p-4 flex items-center justify-between">
         <div class="text-lg font-medium">
-          <Link :href="route('listing.index')">Listings</Link>&nbsp;
+          <Link :href="route('listing.index')">Listings</Link>
         </div>
         <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
-          <Link :href="route('listing.index')">LaraZillow</Link>&nbsp;
+          <Link :href="route('listing.index')">LaraZillow</Link>
         </div>
-        <div>
-          <Link :href="route('listing.create')" class="btn-primary">
-            + New Listing
-          </Link>
+        <div v-if="user" class="flex items-center gap-4">
+          <div class="text-sm text-gray-500">{{ user.name }}</div>
+          <Link :href="route('listing.create')" class="btn-primary">+ New Listing</Link>
+          <div>Logout</div>
+        </div>
+        <div v-else>
+          <Link :href="route('login')">Sign-In</Link>
         </div>
       </nav>
     </div>
   </header>
-
+  
   <main class="container mx-auto p-4 w-full">
     <div v-if="flashSuccess" class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
       {{ flashSuccess }}
@@ -24,13 +27,15 @@
     <slot>Default</slot>
   </main>
 </template>
+
 <script setup>
-  import {computed} from 'vue'
-  import {Link, usePage} from '@inertiajs/vue3'
-
-  const page = usePage()
-  const flashSuccess = computed(() => 
-    page.props.flash.success, 
-  )
-
+import { computed } from 'vue'
+import { Link, usePage } from '@inertiajs/vue3'
+const page = usePage()
+const flashSuccess = computed(
+  () => page.props.flash.success,
+)
+const user = computed(
+  () => page.props.user,
+)
 </script>
