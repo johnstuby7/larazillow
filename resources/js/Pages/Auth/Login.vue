b186a2dcbf00f1a32b59bdeb2176d86655c48ce0
John Stuby <johnstuby7@gmail.com>
2023-04-21 17:46:32 -0600

    Added links for registration

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -2,7 +2,7 @@
   <form @submit.prevent="login">
     <div class="w-1/2 mx-auto">
       <div>
-        <label for="email" class="label">E-mail (username)</label>
+        <label for="email" class="label">E-mail</label>
         <input id="email" v-model="form.email" type="text" class="input" />
         <div v-if="form.errors.email" class="input-error">{{ form.errors.email }}</div>
       </div>
@@ -13,13 +13,19 @@
       </div>
       <div class="mt-4">
         <button class="btn-primary w-full" type="submit">Login</button>
+
+        <div class="mt-2 text-center">
+          <Link :href="route('user-account.create')" class="text-sm text-gray-500">
+            Need an account? Click here
+          </Link>
+        </div>
       </div>
     </div>
   </form>
 </template>
-<script setup>
-import { useForm } from '@inertiajs/vue3'
 
+<script setup>
+import { useForm, Link } from '@inertiajs/vue3'
 const form = useForm({
   email: null,
   password: null,
