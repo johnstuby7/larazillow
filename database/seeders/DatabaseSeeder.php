7ae76957bf88210abd6a0806348baa9a546a25be
John Stuby <johnstuby7@gmail.com>
2023-04-21 22:10:48 -0600

    Improvement for newly registered users, still issue with existing seeded users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
             'is_admin' => true
+            
         ]);
 
         \App\Models\User::factory()->create([
