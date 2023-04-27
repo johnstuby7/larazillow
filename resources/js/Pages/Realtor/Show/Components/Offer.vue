e171a497c54b7870ca9b0a36f4f5f8b9c22f71a7
John Stuby <johnstuby7@gmail.com>
2023-04-27 14:47:49 -0600

    UI improvements, displays sold for each individual realtor listing that is sold

--- resources/js/Pages/Realtor/Show/Components/Offer.vue
+++ resources/js/Pages/Realtor/Show/Components/Offer.vue
@@ -2,10 +2,7 @@
   <Box>
     <template #header>
       Offer #{{ offer.id }} 
-      <span 
-        v-if="offer.accepted_at" 
-        class="dark:bg-green-900 dark:text-green-200 bg-green-200 text-green-900 p-1 rounded-md uppercase ml-1"
-      >
+      <span v-if="offer.accepted_at" class="dark:bg-green-900 dark:text-green-200 bg-green-200 text-green-900 p-1 rounded-md uppercase ml-1">
         accepted
       </span>
     </template>
@@ -28,7 +25,7 @@
       </div>
       <div>
         <Link
-          v-if="notSold"
+          v-if="!isSold"
           :href="route('realtor.offer.accept', { offer: offer.id })"
           class="btn-outline text-xs font-medium" 
           as="button" method="put"
@@ -48,15 +45,12 @@ import { computed } from 'vue'
 const props = defineProps({
   offer: Object,
   listingPrice: Number,
+  isSold: Boolean,
 })
 const difference = computed(
   () => props.offer.amount - props.listingPrice,
 )
 const madeOn = computed(
   () => new Date(props.offer.created_at).toDateString(),
-)
-
-const notSold = computed(
-  () => !props.offer.accepted_at && !props.offer.rejected_at,
 )
 </script>
