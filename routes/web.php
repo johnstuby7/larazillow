1eb7406b3dccdce7a4050e52114d5e914fa29b3e
John Stuby <johnstuby7@gmail.com>
2023-04-27 17:49:29 -0600

    Added in functionality to mark notifications as read to work

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\ListingOfferController;
 use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\NotificationSeenController;
 use App\Http\Controllers\RealtorListingAcceptOfferController;
 use App\Http\Controllers\RealtorListingController;
 use App\Http\Controllers\RealtorListingImageController;
@@ -36,6 +37,10 @@ Route::resource('listing.offer', ListingOfferController::class)
 Route::resource('notification', NotificationController::class)
   ->middleware('auth')
   ->only(['index']);
+
+Route::put('notification/{notification}/seen',
+  NotificationSeenController::class
+)->middleware('auth')->name('notification.seen');
 
 Route::get('login', [AuthController::class, 'create'])
   ->name('login');
