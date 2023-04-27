30ab35456c1fd16fcf37cd3f616da547870c3640
John Stuby <johnstuby7@gmail.com>
2023-04-27 14:41:01 -0600

    Hardened security, made it so that you cant make a bid on a already sold listing and ensure that certain actions are locked behind authentication call

--- app/Http/Controllers/RealtorListingAcceptOfferController.php
+++ app/Http/Controllers/RealtorListingAcceptOfferController.php
@@ -9,14 +9,16 @@ class RealtorListingAcceptOfferController extends Controller
 {
     public function __invoke(Offer $offer)
     {
+        $listing = $offer->listing;
+        $this->authorize('update', $listing);
         // Accept Selected offer
         $offer->update(['accepted_at' => now()]);
 
-        $offer->listing->sold_at = now();
-        $offer->listing->save();
+        $listing->sold_at = now();
+        $listing->save();
 
         // Reject all other offers
-        $offer->listing->offers()->except($offer)
+        $listing->offers()->except($offer)
             ->update(['rejected_at' => now()]);
 
         return redirect()->back()
--- app/Policies/ListingPolicy.php
+++ app/Policies/ListingPolicy.php
@@ -37,8 +37,13 @@ class ListingPolicy
      */
     public function view(?User $user, Listing $listing)
     {
-        return true;
+        if ($listing->by_user_id === $user?->id) {
+            return true;
+        }
+
+        return $listing->sold_at === null;
     }
+    
 
     /**
      * Determine whether the user can create models.
@@ -60,7 +65,8 @@ class ListingPolicy
      */
     public function update(User $user, Listing $listing)
     {
-        return $user->id === $listing->by_user_id;
+        return $listing->sold_at === null
+            && ($user->id === $listing->by_user_id);
     }
 
     /**
