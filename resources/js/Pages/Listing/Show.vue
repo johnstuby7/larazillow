aee91f875643560190f33a0328df8429d1e5104b
John Stuby <johnstuby7@gmail.com>
2023-04-27 10:36:31 -0600

    Added makeoffer component for the individual realtor listing

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -66,6 +66,7 @@
           </div>
         </div>
       </Box>
+      <MakeOffer :listing-id="listing.id" :price="listing.price" />
     </div>
   </div>
 </template>
@@ -77,6 +78,7 @@ import Price from '@/Components/Price.vue'
 import Box from '@/Components/UI/Box.vue'
 import {ref} from 'vue'
 import {useMonthlyPayment} from '@/Composables/useMonthlyPayment'
+import MakeOffer from '@/Pages/Listing/Show/Components/MakeOffer.vue'
 
 const interestRate = ref(2.5)
 const duration = ref(25)
